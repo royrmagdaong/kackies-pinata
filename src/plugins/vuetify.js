7a43f94fd48781e33e84cdcc2c56dc7d2da56273
royrmagdaong <royrmagdaong@example.com>
2021-04-01 21:00:34 +0800

    overrive x-small breakpoints in vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    breakpoint: {
+      thresholds: {
+        xs: 760,
+        sm: 960,
+        md: 1264,
+        lg: 1904,
+      }
+    },
 });
